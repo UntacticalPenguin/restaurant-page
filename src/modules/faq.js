94fc34aa2dad7d01efcc58adbff780b1185fdb7e
UntacticalPenguin <ilja.afonin@gmail.com>
2021-01-03 00:09:20 +0100

    Added alpha version of openable tabs for faq.js, not css-styled and not content-revised

--- src/modules/faq.js
+++ src/modules/faq.js
@@ -1,5 +1,38 @@
-export default function content_faq(x){
-    let element = document.createElement('p');
-    x.appendChild(element);
-    element.textContent = 'Yes, we can make your marriage spicy';
+export default function create_slide_tab(x){
+    let title_div = document.createElement('div');
+    title_div.classList.add('title-div');
+    x.appendChild(title_div);
+  
+    let sub_div = document.createElement('div');
+    title_div.appendChild(sub_div);
+    sub_div.classList.add('sub-div');
+  
+    let title = document.createElement('p');
+    title.textContent = 'Is there something you cant make spicy?';
+    sub_div.appendChild(title);
+  
+    let icon1 = document.createElement('i');
+    icon1.classList.add('gg-chevron-down');
+    sub_div.appendChild(icon1);
+  
+    icon1.addEventListener('click', event =>{
+      let sub_div_answer = document.createElement('div');
+      let answer = document.createElement('p');
+      answer.textContent = 'We dont know lmao';
+      answer.classList.add('answer');
+      sub_div_answer.classList.add('sub-answer');
+      sub_div_answer.appendChild(answer);
+      title_div.appendChild(sub_div_answer);
+      title_div.style.height = '200px';
+      let icon2 = document.createElement('i');
+      icon2.classList.add('gg-chevron-left');
+      sub_div.removeChild(sub_div.lastChild);
+      sub_div.appendChild(icon2);
+      icon2.addEventListener('click', event=>{
+        sub_div.removeChild(sub_div.lastChild);
+        sub_div.appendChild(icon1);
+        title_div.removeChild(title_div.lastChild);
+        title_div.style.height = '100px';
+      });
+    });
   }
